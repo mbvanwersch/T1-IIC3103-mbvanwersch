775e3dd730577bd6b2ab3118a10392e38f37da0d
jpjoublan <mbvanwersch@uc.cl>
2020-03-29 13:35:55 -0300

    arreglos

--- hello_world/views.py
+++ hello_world/views.py
@@ -93,6 +93,8 @@ def show_location(request):
     if len(resident_ids) > 0:
         residents_total = get_multiple_character(resident_ids[:-1])
         if len(resident_ids.split(",")) == 1: # Solo hay 1 elemento
+            print("Hay solo 1 elemento")
+            print(residents_total)
             residents.append({'name' : residents_total['name'], 'id' : residents_total['id']})
         else:
             for res in residents_total:
